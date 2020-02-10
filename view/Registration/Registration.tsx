68d092e72b4838feb5eda1a50a4ef3fcfe1f6217
anton <antonkotik0@gmail.com>
2020-02-10 17:39:02 +0200

    feat: - Added registration form

--- view/Registration/Registration.tsx
+++ view/Registration/Registration.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
 import {View, Text } from "react-native";
+import {Formik} from "formik";
+
+interface RegistrationFormValues {
+    email: string;
+    password: string;
+    confirmPassword: string;
+}
 
 const Registration: React.FC = () => {
+    const initialValues: RegistrationFormValues = {
+        email: '',
+        password: '',
+        confirmPassword: ''
+    };
+
+    const submitRegForm = (form: RegistrationFormValues) => {
+
+    };
+
+
     return <View>
         <Text> Registration: </Text>
+        <Formik
+            initialValues={initialValues}
+            onSubmit={submitRegForm}
+        >
+
+        </Formik>
     </View>
 };
 export default Registration;
