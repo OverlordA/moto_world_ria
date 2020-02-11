4a7e282d8149e71672ba9098f159c0a67ae02c30
anton <antonkotik0@gmail.com>
2020-02-11 12:13:17 +0200

    feat: - Added registration form fields

--- view/Registration/Registration.tsx
+++ view/Registration/Registration.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text } from "react-native";
+import {View, Text, TextInput, Button, StyleSheet} from "react-native";
 import {Formik} from "formik";
 
 interface RegistrationFormValues {
@@ -26,8 +26,37 @@ const Registration: React.FC = () => {
             initialValues={initialValues}
             onSubmit={submitRegForm}
         >
-
+            {({ handleChange, handleBlur, handleSubmit, values, errors }) => (
+                <View>
+                    { errors.email ? <Text style={styles.errorMessage}>{errors.email}</Text>:<Text/>}
+                    <TextInput
+                        onChangeText={handleChange('email')}
+                        onBlur={handleBlur('email')}
+                        value={values.email}
+                    />
+                    { errors.password ? <Text style={styles.errorMessage}>{errors.password}</Text>:<Text/>}
+                    <TextInput
+                        onChangeText={handleChange('password')}
+                        onBlur={handleBlur('password')}
+                        value={values.password}
+                    />
+                    { errors.confirmPassword ? <Text style={styles.errorMessage}>{errors.confirmPassword}</Text>:<Text/>}
+                    <TextInput
+                        onChangeText={handleChange('confirmPassword')}
+                        onBlur={handleBlur('confirmPassword')}
+                        value={values.confirmPassword}
+                    />
+                    <Button onPress={handleSubmit} title="Submit" />
+                </View>
+            )}
         </Formik>
     </View>
 };
+
+const styles = StyleSheet.create({
+   errorMessage: {
+       color: '#ff171c'
+   }
+});
+
 export default Registration;
